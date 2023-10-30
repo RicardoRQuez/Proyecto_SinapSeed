c606aa24f3191a0936394639aee25c137982fb3f
Enjulandia <diegomartinezlienlaf@gmail.com>
2023-10-30 01:12:50 -0300

    vigesimo quinto commit

--- Frontend/src/Componentes/Navbar/Navbar.jsx
+++ Frontend/src/Componentes/Navbar/Navbar.jsx
@@ -1,47 +1,50 @@
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import React from 'react';
 import './Navbar.css';
 
 export function MyNavbar() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const nav = document.querySelector('nav');
+      if (window.pageYOffset > 80) {
+        nav.classList.add('bg-dark', 'shadow');
+      } else {
+        nav.classList.remove('bg-dark', 'shadow');
+      }
+    };
 
-  const nav = document.querySelector('nav');
+    window.addEventListener('scroll', handleScroll);
 
-  window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 80) {
-      nav.classList.add('bg-dark', 'shadow');
-    } else {
-      nav.classList.remove('bg-dark', 'shadow');
-    }
-  });
-  
-  
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // El array vacío asegura que el efecto solo se ejecute una vez, similar a componentDidMount
+
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark p-md3">
       <div className="container">
         <NavLink className="navbar-brand" to="/">SinapSeed</NavLink>
-        <div className="collapse navbar-collapse">
+  
           <ul className="navbar-nav ml-auto">
-              <>
-                <li className="nav-item" >
-                  <NavLink className="nav-link" to="/cursos">Cursos</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/info">Foro</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link noticias" to="/noticias">Noticias</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link inicioSesion" to="/login">Iniciar sesión</NavLink>
-                </li>
-              
-                <li className="nav-item">
-                  <button className="nav-link btn btn-link">Registro</button>
-                </li>
-              
-               </>
+            <>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/cursos">Cursos</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/info">Foro</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/noticias">Noticias</NavLink>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link btn btn-link">Registro</button>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link inicioSesion" to="/login">Iniciar sesión</NavLink>
+              </li>
+            </>
           </ul>
-        </div>
+     
       </div>
     </nav>
   );
--- Frontend/src/Componentes/VistaHome/Portada/Banner.jsx
+++ Frontend/src/Componentes/VistaHome/Portada/Banner.jsx
@@ -5,7 +5,7 @@ export const Banner = () => {
   return (
     <div className="banner-image w-100 vh-100 d-flex">
       <div className="container-fluid texto-principal">
-        <h1>
+        <h1 className="hazSinapSeed">
           ¡Haz <span className="color rosa">SinapSeed!</span><br />
           Sé la mejor versión de ti.
         </h1>
@@ -19,6 +19,53 @@ export const Banner = () => {
           <span className="color rosa">impulsar <br/>tu desarrollo profesional y crecimiento personal.</span>
         </p>
       </div>
+
+      <div className="container datos-portada">
+      <div className="row cuadro text-center">
+        <div className="col">
+          <img src="imágenes/cuadro 1.png" alt="" className="iconito" />
+        </div>
+        <div className="col">
+          <img src="imágenes/cuadro 2.png" alt="" className="iconito" />
+        </div>
+        <div className="col">
+          <img src="imágenes/cuadro 3.png" alt="" className="iconito" />
+        </div>
+        <div className="col">
+          <img src="imágenes/cuadro 4.png" alt="" className="iconito" />
+        </div>
+      </div>
+
+      <div className="row cuadro text-center" style={{ color: '#C54BBF', fontWeight: 'bolder', fontSize: '25px' }}>
+        <div className="col">
+          70+
+        </div>
+        <div className="col">
+          40+
+        </div>
+        <div className="col">
+          5000+
+        </div>
+        <div className="col">
+          100+
+        </div>
+      </div>
+
+      <div className="row cuadro text-center" style={{ color: '#15053b', fontWeight: 600 }}>
+        <div className="col">
+          <p className="texto-cuadro">Cursos, bootcamps y más!</p>
+        </div>
+        <div className="col">
+          <p className="texto-cuadro">Empresas asociadas</p>
+        </div>
+        <div className="col">
+          <p className="texto-cuadro">Egresados con empleo</p>
+        </div>
+        <div className="col">
+          <p className="texto-cuadro">Usuarios al día</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
