57ea9c3d094b00baf04b741347a920990b99f1cf
IvanaaCastillo <ivanaacastillo@gmail.com>
2023-11-02 18:15:47 -0300

    actualizacion cursos

--- Frontend/src/Componentes/VistaCursos/VistaCursos.jsx
+++ Frontend/src/Componentes/VistaCursos/VistaCursos.jsx
@@ -1,58 +1,71 @@
-import React from 'react';
-import './VistaCursos.css';
+import React from "react";
+import styles from "./VistaCursos.module.css";
+import image1 from "./imagenes/image1.png";
 
-
-export function VistaCursos () {
+export const VistaCursos = () => {
   return (
-      <div>
-        VistaCursos
-  
-        <div className="container">
-          {/* ROW 1 */}
-          <div className="row justify-content-center align-items-center g-2">
-            <img src="img/portada.png" alt="" />
+    <>
+      <div className={`${styles.portadaCursos} d-flex`}></div>
+      <div className="row justify-content-center align-items-center g-2">
+        {/* Primera columna */}
+        <div className={`${styles.columnaUno} col-5`}>
+          {/* Primera fila en la primera columna */}
+          <div className="row">
+            <h2 className={styles.tituloGrande}>Full Stack Javascript</h2>{" "}
           </div>
-          {/* ROW 1 */}
-          <br /><br />
-          {/* ROW 2 */}
-          <div className="row justify-content-center align-items-center g-2">
-            <div className="col">
-              <h2 className="titulo_grande">Full Stack Javascript</h2>
-              <br />
-              <span className='titulo_chico'>¿Qué es un Desarrollador Full Stack?</span>
-              <br />
-              <span className='texto_completo'>
-                Es un programador con un perfil técnico muy completo. Conoce toda la cadena de desarrollo de software o páginas web, y maneja varios lenguajes y tecnologías. Los Desarrolladores Full Stack controlan todo lo que un usuario ve cuando utiliza una página web (front-end), como también la parte “escondida”, que no es visible al público y que incluye, entre otras cosas, las bases de datos y las solicitudes de API (back-end).
-                Estos desarrolladores son muy demandados por el mercado y tienen múltiples oportunidades de carrera y aprendizaje.
-              </span>
-              <br /><br />
-              <a className="btn btn-primary" href="#" role="button">Apuntate al curso</a>
-              <a className="btn btn-success" href="#" role="button">Gratis</a>
+          <div className="row">
+            <span className="tituloChico">
+              <b>¿Qué es un Desarrollador Full Stack?</b>
+            </span>
+          </div>
+          <div className="row">
+            <span className="textoCompleto">
+              Domina el desarrollo web Full Stack con el curso de JavaScript de
+              Generation Chile. Aprende con proyectos reales, recibe apoyo
+              personalizado y accede a oportunidades laborales. Con horarios
+              flexibles y una carrera en crecimiento, este curso es para todos,
+              desde principiantes hasta experimentados. Inscríbete hoy y
+              prepárate para una carrera exitosa. No importa si eres un
+              principiante o ya tienes experiencia en programación; este curso
+              está diseñado para todos. Prepara tu camino hacia una emocionante
+              carrera como desarrollador web Full Stack. ¡Inscríbete en el curso
+              de JavaScript Full Stack de Generation Chile y da el siguiente
+              paso hacia tu éxito profesional!
+            </span>
+          </div>
+          <div className="row">
+            <div className="col mt-3">
+              <a className="btn btn-primary ml-3" href="#" role="button">
+                Apúntate al curso
+              </a>
             </div>
-            <div className="col">
-              <center>
-                <img src="img/javascript.png" alt="" width="50%" />
-              </center>
-              <br />
-              <center>
-                <h3><b className="colornota1" >8.9</b>/<b className="colornota2" >10</b></h3>
-              </center>
-              <br />
-              Quieres ingresar al foro con el detalle de los usuarios de esta puntuación? <a href="#">Pincha Acá</a>
+            <div className="col mt-3">
+              <p className={styles.tituloGratis}> Gratis</p>
             </div>
           </div>
-          {/* ROW 2 */}
         </div>
-        <br /><br /><br />
+        <div className="col-1"></div>
+        <div className="col-5 mt-5">
+          <div className={`${styles.imagenCursos}`}>
+            <img src={image1} alt="Descripción de la imagen" />
+          </div>
+          <div className="row mt-3 text-center">
+            {" "}
+            {/* Primera fila */}
+            <h3>
+              <b className="colorNota">8.9/10</b>
+            </h3>
+          </div>
+          <div className="row mt-3">
+            {" "}
+            {/* Segunda fila */}
+            <p>
+              Quieres ingresar al foro con el detalle de los usuarios de esta
+              puntuación? <a href="#">Pincha Acá</a>
+            </p>
+          </div>
+        </div>
       </div>
-    );
-  };
-
-function TableroMedio() {
-  const [count, setCount] = useState(0);
-  return (
-<table>
-  <tr><td width="60%">Titulo</td><td width="40%">Imagen</td></tr>
-</table>
+    </>
   );
-}
+};
