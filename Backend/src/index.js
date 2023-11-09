f5e0f464f42ec1aff748e91ef7f70d9b8dffdd5f
LuiskdmDev <nta.luisedcaroca@gmail.com>
2023-11-09 16:07:13 -0300

    borre todo

--- Backend/src/index.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js';
-
-const PORT = 3000
-app.listen(PORT)
-console.log(`Servidor conectado en el puerto ${PORT}`)
--- Frontend/src/Componentes/VistaCursos/VistaCursos.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './VistaCursos.css'
-
-export const VistaCursos = () => {
-  return (
-    <div>VistaCursos</div>
-  )
-}
