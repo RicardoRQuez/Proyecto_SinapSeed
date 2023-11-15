0cc93c31b2ccbd3cdc32399c9109d02c3143202a
Enjulandia <diegomartinezlienlaf@gmail.com>
2023-11-15 19:07:04 -0300

    asdasd

--- Backend/src/routes/usuarios.routes.js
+++ Backend/src/routes/usuarios.routes.js
@@ -1,14 +1,15 @@
 import {Router} from 'express';
 import {signup, login, findAll, findAllUserId, updateUserById, deleteUserById}  from '../controllers/users.controllers.js'
 import {emitToken} from '../middleware/emitToken.js'
+import {verifyToken} from '../middleware/verifyToken.js'
 
 const router = Router()
 
 router.post("/login", emitToken, login);
 router.post("/signup", signup);
-router.get("/user", findAll)
-router.get("/user/:id", findAllUserId)
+router.get("/user", verifyToken, findAll)
+router.get("/user/:id",  findAllUserId)
 router.patch("/user/:id", updateUserById)
-router.delete("/user/:id", deleteUserById )
+router.delete("/user/:id", verifyToken, deleteUserById )
 
 export default router;
